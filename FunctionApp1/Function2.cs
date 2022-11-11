4a8cfa77fe9884c12b9259f7048373abac071f9c
Risto Ikonen <ristoikonen@gmail.com>
2022-11-11 13:42:14 +1100

    Add project files.

---
+++ FunctionApp1/Function2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class Function2
+    {
+        [FunctionName("Function2")]
+        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "xxxxxxxxxxxxxxxxxxxxxxx")] Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
