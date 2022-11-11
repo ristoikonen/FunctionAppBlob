2758abc5d8da0371fefd381c3256f97817fbb608
Risto Ikonen <ristoikonen@gmail.com>
2022-11-11 13:46:26 +1100

    Add return of dummy data object.

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -18,6 +18,23 @@ namespace FunctionApp1
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processing a request.");
+            /*
+             * 
+             * ALTERNATIVE - GET porductas modls id and return dummy data object
+            // Retrieve the model id from the query string
+            string model = req.Query["model"];
+
+            // If the user specified a model id, find the details of the model of watch
+            if (model != null)
+            {
+                // Use dummy data for this example
+                dynamic watchinfo = new { Manufacturer = "abc", CaseType = "Solid", Bezel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
+
+                return (ActionResult)new OkObjectResult($"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}");
+            }
+            return new BadRequestObjectResult("Please provide a watch model in the query string"); 
+             
+             */
 
             string name = req.Query["name"];
 
